e3dca928941f97f5b44a09ec355fcae7ef2873b9
alessandro vezzoli <alessandro.vezzoli@vp360.eu>
2024-02-06 17:53:26 +0100

    finish work

--- src/components/AppMailList.vue
+++ src/components/AppMailList.vue
@@ -1,3 +1,30 @@
-<template><h1>ciao</h1></template>
-<script setup></script>
+<template>
+  <h1>LISTA EMAIL</h1>
+  <ul v-if="listmail.length == 10">
+    <li v-for="email in listmail" :key="email">{{ email }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "AppMailList",
+
+  data() {
+    return {
+      listmail: [],
+    };
+  },
+  created() {
+    for (let index = 0; index < 10; index++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((responde) => {
+          const mail = responde.data.response + index;
+          this.listmail.push(mail);
+          console.log(mail);
+        })
+        .catch(() => {});
+    }
+  },
+};
+</script>
 <style scoped></style>
